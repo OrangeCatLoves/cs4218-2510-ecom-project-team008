f9de0c2d444c59a568c528b2d458d3dc69ce1a78
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2025-10-13 22:49:42 +0800

    update register

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -35,7 +35,7 @@ const Register = () => {
       }
     } catch (error) {
       console.log(error);
-      toast.error(error.response.data.message);
+      toast.error(error.response?.data?.message || "Something went wrong");
     }
   };
 
